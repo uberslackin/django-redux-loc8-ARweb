0e78c3458a1a26eec269c8dae686fdca9fb35e3d
G. Willson <greg@biomassiv.es>
2017-02-07 02:33:24 -0700

    Update agent.js

--- loc8-react-redux-front-end/src/agent.js
+++ loc8-react-redux-front-end/src/agent.js
@@ -5,7 +5,9 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://127.0.0.1:8000/api';
+
+const API_ROOT = 'https://conduit.productionready.io/api';
+//const API_ROOT = 'http://127.0.0.1:8000/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
